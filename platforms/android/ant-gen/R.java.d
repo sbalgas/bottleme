d4504b9540911a3d8405a0635223a0ca7d605640
Jorge Riera <jorge.riera9@gmail.com>
2014-03-18 14:56:21 -0300

    cambiando transicion slidefade por fade por mejor rendimient0

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -6,4 +6,9 @@
 /home/yorx/Proyectos/bottleme/platforms/android/res/drawable-hdpi/icon.png \
 /home/yorx/Proyectos/bottleme/platforms/android/res/xml/config.xml \
 /home/yorx/Proyectos/bottleme/platforms/android/res/values/strings.xml \
+/home/yorx/Proyectos/bottleme/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
+/home/yorx/Proyectos/bottleme/platforms/android/ant-build/res/drawable-ldpi/icon.png \
+/home/yorx/Proyectos/bottleme/platforms/android/ant-build/res/drawable-mdpi/icon.png \
+/home/yorx/Proyectos/bottleme/platforms/android/ant-build/res/drawable/icon.png \
+/home/yorx/Proyectos/bottleme/platforms/android/ant-build/res/drawable-hdpi/icon.png \
 /home/yorx/Proyectos/bottleme/platforms/android/ant-build/AndroidManifest.xml \
