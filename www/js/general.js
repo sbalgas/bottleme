6d44111889783737424dbe5155318d43e4257bc8
Leandro Ortega <leandro@g-ortega.com.ar>
2014-04-13 16:36:06 -0300

    Eliminado el login viejo Se empezó con el login viejo anexado al home. Se reestructuró el formulario de registro.
    Signed-off-by: Leandro Ortega <leandro@g-ortega.com.ar>

--- www/js/general.js
+++ www/js/general.js
@@ -1,11 +1,22 @@
+var Wimg = 0;
+var Himg = 0;
+
 $(document).on("pageshow", "#registro", function(){
-	var Wimg = 2598;
-	var Himg = 831;
-	$('.wave').css('height', ($(window).width() * Himg / Wimg) + 'px');
+	Wimg = 2598;
+	Himg = 831;
+	screen_change();
 });
 
 $(document).on("pageshow", "#inicio", function(){
-	var Wimg = 2598;
-	var Himg = 1689;
-	$('.wave').css('height', ($(window).width() * Himg / Wimg) + 'px');
+	Wimg = 2598;
+	Himg = 1689;
+	screen_change();
 });
+
+$(window).resize(function() {
+ 	screen_change();
+});
+
+function screen_change(){
+	$('.wave').css('height', ($(window).width() * Himg / Wimg) + 'px');
+}
